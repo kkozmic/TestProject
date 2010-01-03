07f6bf8b13c890788b6cf238cf4266a78ff0f956
kkozmic <krzysztof@kozmic.pl>
2010-01-03 11:29:06 +0100

    - added sketch of initial model and empty test project

---
+++ src/Wawel.DomainModel/Benchmark.cs
@@ -0,0 +1,34 @@
+﻿namespace Wawel.DomainModel
+{
+	using System;
+	using System.Collections.Generic;
+
+	using Castle.ActiveRecord;
+
+	[ActiveRecord]
+	public class Benchmark
+	{
+		private Guid id;
+
+		[PrimaryKey(Access = PropertyAccess.NosetterCamelcase)]
+		public Guid Id
+		{
+			get { return id; }
+		}
+
+		[Property]
+		public string ApplicationName { get; set; }
+
+		[Property]
+		public string ApplicationVersion { get; set; }
+
+		[Property]
+		public Uri Website { get; set; }
+
+		[Property(ColumnType = "StringClob")]
+		public string Description { get; set; }
+
+		[HasMany(Inverse = true, Cascade = ManyRelationCascadeEnum.AllDeleteOrphan)]
+		public IList<BenchmarkResults> Results { get; set; }
+	}
+}
---
+++ src/Wawel.DomainModel/BenchmarkResults.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Wawel.DomainModel
+{
+	using Castle.ActiveRecord;
+
+	[ActiveRecord]
+	public class BenchmarkResults
+	{
+		private Guid id;
+
+		[PrimaryKey(Access = PropertyAccess.NosetterCamelcase)]
+		public Guid Id
+		{
+			get { return id; }
+		}
+
+		[BelongsTo("BenchmarkId")]
+		public Benchmark Benchmark { get; set; }
+
+	}
+}
